5d0034b4beb7e900be740635154ffef9d7a74b9f
BriWilliams92 <96026988+BriWilliams92@users.noreply.github.com>
2022-02-04 00:37:55 -0500

    Add files via upload

---
+++ AdminDashboard/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Air Jordan 1 High OG "Chicago" & "Bred"',
+        productNumber: '25831',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Air Jordan 1 High OG "Black Toe"',
+        productNumber: '47492',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Off-White Nike Air Jordan IV 2020',
+        productNumber: '43726',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Air Jordan 5 "Raging Bull"',
+        productNumber: '74996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Air Jordan VII "Bordeaux" 1992',
+        productNumber: '22641',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Air Jordan 11 "Bred"',
+        productNumber: '83655',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Air Jordan XII "Taxi" 1996',
+        productNumber: '00864',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
